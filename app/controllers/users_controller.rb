370995b2d5e18ef6f8dfe0b9ac5398044c2df637
anil pothuri <pothurianil.naidu@extwebtech.in>
2022-11-15 18:58:33 +0530

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,19 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params)
      if @user.save 
-     	flash[:success] = "user created succefully"
+     	flash[:success] = "Welcome to the Sample App!"
+     	session[user_id] = @user.id
      	redirect_to root_path
+     else
+     	byebug
+     	render :new
+
+     	flash[:danger] = "something wrong"
      end 
+  end
+  private 
+
+  def user_params 
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
