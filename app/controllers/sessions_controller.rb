bf6deabaa44173bd7203abfa90c813ad3bbb0d61
anil pothuri <pothurianil.naidu@extwebtech.in>
2022-11-16 11:00:10 +0530

    login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,26 @@
 class SessionsController < ApplicationController 
 
- def home
-    
- end
+   def home
+        
+   end
+   def new 
+   end
+   def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+         session[:user_id] = user.id
+         flash[:success] = 'login successfully'
+         redirect_to user
+
+      else
+         flash.now[:danger] = 'Invalid email/password combination'
+         render 'new'
+      end
+   end
+
+   def destroy 
+      @current_user = nil
+      render 'new'
+   end
+
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
      if @user.save 
      	flash[:success] = "Welcome to the Sample App!"
-     	session[user_id] = @user.id
+     	session[:user_id] = @user.id
      	redirect_to root_path
      else
      	byebug
