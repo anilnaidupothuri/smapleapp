eb37538e37aa57e5613cd7d5f9064ecf5690a915
anil pothuri <pothurianil.naidu@extwebtech.in>
2022-11-16 12:30:06 +0530

    remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_105947) do
+ActiveRecord::Schema.define(version: 2022_11_16_054510) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.string "password_confirmation"
+    t.string "remember_digest"
+    t.datetime "timestamp"
   end
 
 end
